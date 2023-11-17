d2f81caa285d35c924cf10f71498ced54698c3d1
sergiusd <sergiusd@ya.ru>
2023-11-17 12:37:08 +0400

    Build deb package and README.md.

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,16 @@
 set -ex
 
-TGZ_FIRST="$(ls -1 ../excalidraw/bin/*.tgz | head -1)"
-if [ ! "" = "$TGZ_FIRST" ]; then
-  TGZ="file://$(realpath $TGZ_FIRST)"
-else
+TGZ_FIRST=""
+if [ -d ../excalidraw ]; then
+  TGZ_FIRST="$(ls -1 ../excalidraw/bin/*.tgz | head -1)"
+  if [ ! "" = "$TGZ_FIRST" ]; then
+    TGZ="file://$(realpath $TGZ_FIRST)"
+  fi
+fi
+if [ "" = "$TGZ_FIRST" ]; then
   read -p "Enter path to package: " TGZ
 fi
+
 sed -i '' "s#\"@jitsi/excalidraw\":.*#\"@jitsi/excalidraw\": \"$(echo $TGZ | xargs)\",#" package.json
 rm -rf node_modules/@jitsi/excalidraw package-lock.json
 npm cache ls @jitsi/excalidraw | npm cache clean --force
