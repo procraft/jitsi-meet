a99faed817db2fe830ecb46abaf9d6631197befb
sergiusd <sergiusd@ya.ru>
2023-11-17 12:18:29 +0400

    Build deb package and README.md.

--- bin/README.md
+++ bin/README.md
@@ -2,6 +2,6 @@
 
 Для сбоки с excalidraw, который умеет шарить картинки нужно собрать deb пакет:
 * в репе https://github.com/procraft/excalidraw создаем архив командой `make our-build`
-* в `package.json` для зависимости `@jitsi/excalidraw` прописываем путь к созданному архиву
 * запускаем сборку deb-пакета командой `make our-build`
+* вводим путь к архиву пакета @jitsi/excalidraw
 * созданный deb-пакет накатываем на стандарный обрыз для jitsi-web сервиса
--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,16 @@
 set -ex
 
-rm -rf node_modules/@jitsi/excalidraw
+TGZ_FIRST="$(ls -1 ../excalidraw/bin/*.tgz | head -1)"
+if [ ! "" = "$TGZ_FIRST" ]; then
+  TGZ="file://$(realpath $TGZ_FIRST)"
+else
+  read -p "Enter path to package: " TGZ
+fi
+sed -i '' "s#\"@jitsi/excalidraw\":.*#\"@jitsi/excalidraw\": \"$(echo $TGZ | xargs)\",#" package.json
+rm -rf node_modules/@jitsi/excalidraw package-lock.json
+npm cache ls @jitsi/excalidraw | npm cache clean --force
 npm i
 make
 docker-compose -p jitsi-meet-deb-build -f bin/docker-compose.yml up --build
+git checkout HEAD -- package-lock.json package.json
 echo "Use $(pwd)/bin/deb files"
