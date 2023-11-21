ffafaaba54e468a7be7208f64932c0d019e6aebe
sergiusd <sergiusd@ya.ru>
2023-11-21 12:39:23 +0400

    Added js version to deb package.

--- bin/build.sh
+++ bin/build.sh
@@ -2,10 +2,14 @@ set -ex
 
 . ./bin/set-excalidraw.sh
 
+version="$(date +%s)"
+
 rm -rf node_modules/@jitsi/excalidraw package-lock.json
 npm i
 make
-sed -i '' "s/jitsi-meet-web (1.0.1-1)/jitsi-meet-web \(1.0.$(date +%s)\)/" debian/changelog
+sed -i '' "s/jitsi-meet-web (1.0.1-1)/jitsi-meet-web \(1.0.$version\)/" debian/changelog
+sed -i '' "s#libs/lib-jitsi-meet.min.js\?v=[^\"]*#libs/lib-jitsi-meet.min.js?v=$version#" index.html
+sed -i '' "s#libs/app.bundle.min.js\?v=[^\"]*#libs/app.bundle.min.js?v=$version#" index.html
 docker-compose -p jitsi-meet-deb-build -f bin/docker-compose.yml up --build
-git checkout HEAD -- package-lock.json package.json debian/changelog
+git checkout HEAD -- package-lock.json package.json debian/changelog index.html
 echo "Use $(pwd)/bin/deb files"
